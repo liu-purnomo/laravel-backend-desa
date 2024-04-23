d6bf7528d4cc8219f0cbe3f0c15f1589b594916b
liu-purnomo <kontak.liu@gmail.com>
2024-04-24 00:35:04 +0700

    feat: first commit

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+//route login
+Route::post('/login', [App\Http\Controllers\Api\Auth\LoginController::class, 'index']);
+
+//group route with middleware "auth"
+Route::group(['middleware' => 'auth:api'], function() {
+
+    //logout
+    Route::post('/logout', [App\Http\Controllers\Api\Auth\LoginController::class, 'logout']);
+
+});
